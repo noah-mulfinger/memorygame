4de874910ad3130a5d248980eb769fa0eb9a5d75
Noah Mulfinger <noah.mulfinger@gmail.com>
2015-04-05 18:40:29 +0200

    second test, modifying gameview

--- app/src/main/java/hu/ait/android/noah/memorygame/view/GameView.java
+++ app/src/main/java/hu/ait/android/noah/memorygame/view/GameView.java
@@ -1,7 +1,15 @@
 package hu.ait.android.noah.memorygame.view;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
 /**
  * Created by noah on 4/5/15.
  */
-public class GameView {
+public class GameView extends GridView{
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 }
